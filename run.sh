9557e66a0ce4cb0e23a5ff52412a0aff969b83fb
shuailiu <shuailiu@aibee.com>
2020-06-29 15:47:58 +0800

    MOD:update the cmake file

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 
+#================================================================
+#   Copyright (C) 2020 Sangfor Ltd. All rights reserved.
+#   
+#       FileName：run.sh
+#         Author：Joshua_Zero@outlook.com
+#     CreateTime：2020-06-28 16:14:33
+#    Describtion：
+# Last Modified: 2020-06-28 16:14:33
+#
+#================================================================
+set -ex
+
+path=`pwd`
+cd ${path}/build
+cmake ..  
+
+
